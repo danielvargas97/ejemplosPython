d41da64a76188aec7abf4a07da66a326aab6cc47
danielvargas97 <danielvargas999@gmail.com>
2017-03-28 23:07:44 -0500

    Add files via upload

---
+++ Hold.py
@@ -0,0 +1,14 @@
+#Ejemplo de Hold#
+
+class Pato(object):
+    def __init__(self,sonido):
+        self.sonido = sonido
+
+    def getSonido(self):
+        return self.sonido
+
+    
+if __name__ == "__main__"
+    p = Pato()
+    print(p.getSonido())
+    
